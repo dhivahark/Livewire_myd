c820e8e8d0ec781994ffe2a66222de9bcea98780
dhivahark <dhivahardhivahar7366@gmail.com>
2025-02-22 16:44:00 +0530

    Add files via upload

---
+++ certification programs/card slider.js
@@ -0,0 +1,44 @@
+
+/*..........................................slider....................................................*/
+
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 30,
+  
+    // Pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+  
+    // Navigation
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // Responsive
+    breakpoints: {
+      0: {
+        slidesPerView: 1
+      },
+      768: {
+        slidesPerView: 2
+      },
+      1024: {
+        slidesPerView: 3
+      }
+    }
+  });
+  
+  
+  /*............................................accordion................................................*/
+
+  const faqs = document.querySelectorAll(".faq");
+  faqs.forEach((faq) => {
+    faq.addEventListener('click', () => {
+      faq.classList.toggle("active");
+    });
+  });
